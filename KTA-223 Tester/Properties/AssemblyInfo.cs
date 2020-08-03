90d6f7d19415901d04cbe46b18f3e43896686f76
Adam Johnson <adamj537@gmail.com>
2020-08-03 14:50:31 -0500

    Adding files created during the conversion to C#.

---
+++ KTA-223 Tester/Properties/AssemblyInfo.cs
@@ -0,0 +1,50 @@
+// VBConversions Note: VB project level imports
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Drawing;
+using System.Diagnostics;
+using System.Data;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Windows.Forms;
+// End of VB project level imports
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using WindowsApplication1;
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly:AssemblyTitle("KTA-223 Tester")]
+[assembly:AssemblyDescription("This program communicates with Arduino-based KTA-223.")]
+[assembly:AssemblyCompany("")]
+[assembly:AssemblyProduct("WindowsApplication1")]
+[assembly:AssemblyCopyright("Copyright ©  2009")]
+[assembly:AssemblyTrademark("")]
+
+[assembly:ComVisible(false)]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly:Guid("18c80c62-841a-4a7a-a66d-0e566f2c8c23")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")>
+
+[assembly:AssemblyVersion("1.0.0.0")]
+[assembly:AssemblyFileVersion("1.0.0.0")]
+
