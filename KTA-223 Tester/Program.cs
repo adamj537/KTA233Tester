9940891ee5c6bd27c37f1edba346b8b0bfa2c707
Adam Johnson <adamj537@gmail.com>
2022-04-18 19:08:06 -0500

    Upgraded to .NET 6.0

--- KTA-223 Tester/Program.cs
+++ KTA-223 Tester/Program.cs
@@ -12,6 +12,7 @@ namespace KTA233Tester
 		static void Main()
 		{
 			Application.EnableVisualStyles();
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new FormMain());
 		}
